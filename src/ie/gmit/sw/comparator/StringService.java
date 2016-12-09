59f4205d28fa6f201cbf3c855d6414f3e8155a1c
Martinc94 <martinc94@live.co.uk>
2016-12-09 19:10:13 +0000

    client needs copy of Interfaces

---
+++ src/ie/gmit/sw/comparator/StringService.java
@@ -0,0 +1,19 @@
+package ie.gmit.sw.comparator;
+
+/*			public Resultator compare(String s, String t, String algo) throws RemoteException;
+* 
+*			where s and t are the two strings to compare, algo is the string matching algorithm to
+*			use and Resultator is a remote object reference that allows the RMI service provider
+*			to push an asynchronous response to the RMI requestor (a pass by reference from the
+*			service provider to the service requestor). The string comparison service should
+*			delegate calls to an instance of the algorithm specified in the request, running in a
+*			separate thread. Before comparing the strings, the string algorithm thread should be put
+*			to sleep for a time, i.e. Thread.sleep(1000), to slow the service down and simulate a
+*			real asynchronous service.
+*/
+
+import java.rmi.*;
+
+public interface StringService extends Remote{
+	public Resultator compare(String s, String t, String algo) throws RemoteException;
+}//end StringService
