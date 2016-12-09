f8cb1c75162137b370cf14fd71de93bdc0beff21
Martinc94 <martinc94@live.co.uk>
2016-12-09 19:10:51 +0000

    updated ServiceHandler

--- src/ie/gmit/sw/ServiceHandler.java
+++ src/ie/gmit/sw/ServiceHandler.java
@@ -1,16 +1,23 @@
 package ie.gmit.sw;
 
 import java.io.*;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import ie.gmit.sw.comparator.Job;
+import ie.gmit.sw.comparator.QueueManager;
+
 public class ServiceHandler extends HttpServlet {
 	private String remoteHost = null;
-	private static long jobNumber = 0;
+	private volatile static long jobNumber = 0;
+	QueueManager m;
 
 	public void init() throws ServletException {
 		ServletContext ctx = getServletContext();
 		remoteHost = ctx.getInitParameter("RMI_SERVER"); //Reads the value from the <context-param> in web.xml
+		//manager for Queues
+		m = new QueueManager();
 	}
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -22,22 +29,34 @@ public class ServiceHandler extends HttpServlet {
 		String s = req.getParameter("txtS");
 		String t = req.getParameter("txtT");
 		String taskNumber = req.getParameter("frmTaskNumber");
+		
+		Boolean complete=false;
+		String distance="";
 
 
 		out.print("<html><head><title>Distributed Systems Assignment</title>");		
 		out.print("</head>");		
 		out.print("<body>");
 		
+		//if there is no taskNo then a task is created
 		if (taskNumber == null){
 			taskNumber = new String("T" + jobNumber);
 			jobNumber++;
-			//Add job to in-queue
+			
+			//create Job object from the request variables and jobNumber
+			Job j = new Job(s, t, algorithm, taskNumber);
+			
+			//Add job to in-queue blockingQueue
+			m.add(j);
 		}else{
 			//Check out-queue for finished job
-		}
-		
-		
-		
+			complete=m.isComplete(taskNumber);
+			
+			if(complete){
+				distance=m.getResult(taskNumber);
+			}//end if
+		}//else
+			
 		out.print("<H1>Processing request for Job#: " + taskNumber + "</H1>");
 		out.print("<div id=\"r\"></div>");
 		
@@ -46,21 +65,10 @@ public class ServiceHandler extends HttpServlet {
 		out.print("<br>Algorithm: " + algorithm);		
 		out.print("<br>String <i>s</i> : " + s);
 		out.print("<br>String <i>t</i> : " + t);
-		out.print("<br>This servlet should only be responsible for handling client request and returning responses. Everything else should be handled by different objects.");
-		out.print("Note that any variables declared inside this doGet() method are thread safe. Anything defined at a class level is shared between HTTP requests.");				
-		out.print("</b></font>");
-
-		out.print("<P> Next Steps:");	
-		out.print("<OL>");
-		out.print("<LI>Generate a big random number to use a a job number, or just increment a static long variable declared at a class level, e.g. jobNumber.");	
-		out.print("<LI>Create some type of an object from the request variables and jobNumber.");	
-		out.print("<LI>Add the message request object to a LinkedList or BlockingQueue (the IN-queue)");			
-		out.print("<LI>Return the jobNumber to the client web browser with a wait interval using <meta http-equiv=\"refresh\" content=\"10\">. The content=\"10\" will wait for 10s.");	
-		out.print("<LI>Have some process check the LinkedList or BlockingQueue for message requests.");	
-		out.print("<LI>Poll a message request from the front of the queue and make an RMI call to the String Comparison Service.");			
-		out.print("<LI>Get the <i>Resultator</i> (a stub that is returned IMMEDIATELY by the remote method) and add it to a Map (the OUT-queue) using the jobNumber as the key and the <i>Resultator</i> as a value.");	
-		out.print("<LI>Return the result of the string comparison to the client next time a request for the jobNumber is received and the <i>Resultator</i> returns true for the method <i>isComplete().</i>");	
-		out.print("</OL>");	
+		
+		if(complete){
+            out.print("<br>Distance was calculated as: " + distance);
+        }
 		
 		out.print("<form name=\"frmRequestDetails\">");
 		out.print("<input name=\"cmbAlgorithm\" type=\"hidden\" value=\"" + algorithm + "\">");
@@ -76,8 +84,6 @@ public class ServiceHandler extends HttpServlet {
 		out.print("</script>");
 				
 		//You can use this method to implement the functionality of an RMI client
-		
-		//
 	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
